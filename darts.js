44b49fee4f375d3c5f8672f0a44f93926cf272de
Adam Paul <adam.paul@live.com>
2021-07-02 11:20:33 -0230

    Changed structure and scope

--- darts.js
+++ darts.js
@@ -1,5 +1,16 @@
-/* Author: Adam
-This is the JavaScript for my webpage.
+/*
+    This page will create user classes
 */
-
-//
+function newPlayer() {
+    let newUser = document.getElementById("createPlayer");
+    newUser.addEventListener("click", () => {
+        let newPlayerInput = document.createElement("input");
+        newPlayerInput.setAttribute("type", "text");
+    })
+}
+class User {
+    constructor(name, nickName) {
+        this.name = name;
+        this.nickName = nickName;
+    }
+}
