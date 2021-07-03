80df0d11236a9f908b6a83b1b546b004d7c027a1
Adam Paul <adam.paul@live.com>
2021-07-03 18:21:00 -0230

    connect with mysql

---
+++ startServer.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password"
+});
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+  });
