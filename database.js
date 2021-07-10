d60feea065de3e36ecffe5345e1d67a1afd01530
Adam Paul <adam.paul@live.com>
2021-07-10 16:30:22 -0230

    added .env, started database

--- database.js
+++ database.js
@@ -1,18 +1,27 @@
 const mysql = require('mysql2');
+const env = require('dotenv').config();
+const express = require('express');
+const app = express();
 
-const con = mysql.createConnection ({
-  host: "localhost",
-  user: "root",
-  password: "Password123!",
-  database: "players",
+const db = mysql.createPool ({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
 });
 
-// Test Connection
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Server Connected!");
+app.use(express.json());
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port ${listener.address().port}`)
 });
 
-// con.end();
-
-module.exports = con;
+app.get('/allPlayers', (req, res) => {
+  const sql = `SELECT * FROM ${process.env.DB}.players;`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  })
+})
