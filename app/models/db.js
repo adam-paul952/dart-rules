c5426542ced165423724764e180934698f900977
Adam Paul <adam.paul@live.com>
2022-01-22 13:13:04 -0330

    included cross-env

--- app/models/db.js
+++ app/models/db.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql2");
 const env = require("dotenv").config();
 
+let databaseName;
+
+if (process.env.NODE_ENV === "test") {
+  databaseName = process.env.DB_TEST;
+} else {
+  databaseName = process.env.DB;
+}
+
 // Create connection to the database
 const connection = mysql.createConnection({
   host: process.env.DB_HOST, // Replace with your host
   user: process.env.DB_USER, // Replace with your user
   password: process.env.DB_PASSWORD, //  Replace with your password
-  database: process.env.DB, // "dartscoreboardserver"
+  database: databaseName, // "dartscoreboardserver"
 });
 
 connection.connect((error) => {
